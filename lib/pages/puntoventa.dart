66fdf46204959b261eab5e1a32c235ece1ee6aae
LitzyDiaz0 <2137000110@utna.edu.mx>
2024-11-27 00:00:06 -0600

    creacion de carpeta models y services

--- lib/pages/puntoventa.dart
+++ lib/pages/puntoventa.dart
@@ -7,117 +7,147 @@ class PuntoDeVentaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(144, 73, 10, 1), // Color café
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back), // Flecha para volver
-          onPressed: () {
-            // Navegar hacia atrás
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const AdminPage(),
-              ),
-            );
-          },
-        ),
-        title: Image.asset(
-          'assets/img/logo.png', // Logo en el centro
-          height: 40,
-        ),
-        centerTitle: true,
-        toolbarHeight: 80, // Aumentar la altura para diseño
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Título debajo del encabezado
-            const Text(
-              'El Rincón del Pan',
-              style: TextStyle(
-                fontSize: 36,
-                fontFamily: 'Italianno',
-                color: Color.fromRGBO(144, 73, 10, 1),
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            // Input para buscar pan
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Nombre del pan a vender',
-                prefixIcon: const Icon(
-                  Icons.bakery_dining, // Icono de pan
-                  color: Color.fromRGBO(144, 73, 10, 1),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            // Botones en la misma fila
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // Botón Buscar Pan
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Lógica para buscar pan
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      side: const BorderSide(
-                        color: Colors.black, // Borde negro
-                        width: 1,
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          // Contenido principal
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // AppBar con color café
+              Container(
+                height: 80, // Altura del encabezado
+                color: const Color.fromARGB(255, 135, 71, 15), // Color café
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 15.0), // Espacio adicional arriba del ícono
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ), // Flecha para volver
+                        iconSize: 40, // Tamaño del ícono de la flecha
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const AdminPage(),
+                            ),
+                          );
+                        },
                       ),
                     ),
-                  ),
-                  icon: const Icon(
-                    Icons.search, // Icono de lupa
-                    color: Colors.black,
-                  ),
-                  label: const Text(
-                    'Buscar Pan',
-                    style: TextStyle(color: Colors.black),
+                  ],
+                ),
+              ),
+              // Espacio para acomodar el logo que sobresale
+              const SizedBox(height: 50),
+              // Título debajo del logo
+              const Center(
+                child: Text(
+                  'El Rincón del Pan',
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontFamily: 'Italianno',
+                    color: Color.fromRGBO(96, 60, 30, 1),
                   ),
                 ),
-                // Botón Ventas del Día
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // Lógica para ver ventas del día
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 12, horizontal: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      side: const BorderSide(
-                        color: Colors.black, // Borde negro
-                        width: 1,
-                      ),
+              ),
+              const SizedBox(height: 30),
+              // Input para buscar pan
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nombre del pan a vender',
+                    prefixIcon: const Icon(
+                      Icons.bakery_dining, // Icono de pan
+                      color: Color.fromRGBO(144, 73, 10, 1),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
                   ),
-                  icon: const Icon(
-                    Icons.receipt_long, // Icono para ventas
-                    color: Colors.black,
-                  ),
-                  label: const Text(
-                    'Ventas del Día',
-                    style: TextStyle(color: Colors.black),
-                  ),
                 ),
-              ],
+              ),
+              const SizedBox(height: 25),
+              // Botones en la misma fila
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // Botón Buscar Pan
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Lógica para buscar pan
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 12, horizontal: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: const BorderSide(
+                            color: Colors.black, // Borde negro
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                      icon: const Icon(
+                        Icons.search, // Icono de lupa
+                        color: Colors.black,
+                      ),
+                      label: const Text(
+                        'Buscar Pan',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    // Botón Ventas del Día
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Lógica para ver ventas del día
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 12, horizontal: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: const BorderSide(
+                            color: Colors.black, // Borde negro
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                      icon: const Icon(
+                        Icons.receipt_long, // Icono para ventas
+                        color: Colors.black,
+                      ),
+                      label: const Text(
+                        'Ventas del Día',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          // Imagen del logo que sobresale
+          Positioned(
+            top: 30,
+            left:
+                MediaQuery.of(context).size.width / 2 - 50, // Centrar la imagen
+            child: Image.asset(
+              'assets/img/logo.png',
+              height: 110,
+              width: 110,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
